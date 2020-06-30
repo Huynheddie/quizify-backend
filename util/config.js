a6e0b09901d1777e48521de17840970b2413091e
huynheddie <huynheddie@yahoo.com>
2020-06-30 15:20:26 -0700

    Changed local urls

--- util/config.js
+++ util/config.js
@@ -1,7 +1,7 @@
 let AUTHZ_CREDENTIALS = {
     // REDIRECT_URI: 'http://localhost:3000/',
     REDIRECT_URI: 'https://huynheddie.github.io/quizify',
-    BACKEND_REDIRECT_URI: 'http://localhost:3001/callback',
+    BACKEND_REDIRECT_URI: 'https://quizify-backend.herokuapp.com/callback',
     SCOPES: ['user-read-private',
              'user-read-playback-state',
              'user-modify-playback-state',
